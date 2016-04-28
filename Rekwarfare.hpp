bbb5798c23c391f4c966193ef71ac69ecdfafb3e
rodrun <jmendg314@gmail.com>
2016-04-28 18:51:43 -0400

    Change extension to .hpp for format reasons

---
+++ Rekwarfare.hpp
@@ -0,0 +1,18 @@
+#ifndef REKWARFARE_H
+#define REKWARFARE_H
+
+#include "audio/SoundPlayer.hpp"
+#include "graphics/Window.hpp"
+#include "graphics/Drawer.hpp"
+#include "net/Client.hpp"
+#include "net/NetTypes.hpp"
+#include "net/Protocol.hpp"
+#include "net/Server.hpp"
+#include "net/Socket.hpp"
+#include "util/Configuration.hpp"
+#include "util/FPSCounter.hpp"
+#include "util/UFile.h"
+#include "util/Ujson.h"
+#include "util/Ustring.h"
+
+#endif /* REKWARFARE_H */
