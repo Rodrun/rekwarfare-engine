08125ef5bcd27de82669d2176a3a2285febbe24a
rodrun <jmendg314@gmail.com>
2016-04-28 20:09:39 -0400

    Add test

--- test/t1/main.cpp
+++ test/t1/main.cpp
@@ -1,4 +1,25 @@
+#include <iostream>
+#include "Rekwarfare.hpp"
+
+using namespace rekwarfare;
 
 int main() {
+    Window window("t1", REKWARFARE_WINDOWPOS_UNDEF, REKWARFARE_WINDOWPOS_UNDEF,
+        800, 640);
+
+    Texture t;
+    bool loaded = loadTexture(t, "test/resource/t1.png");
+    if (!loaded) {
+        std::cout << "t1.png couldn't be loaded!" << std::endl;
+        return 1;
+    }
+
+    while (window.isRunning()) {
+        window.clear();
+
+        drawTexture(t, 0, 0, window.getWidth(), window.getHeight(), 0, NO_COLOR);
+
+        window.update();
+    }
     return 0;
 }
