7b699456bc7a103dcd8821584148712047a8f828
rodrun <rodrun@github.com>
2016-05-11 22:20:18 -0400

    Update test (font testing fail) In CMakeLists.txt: - Encapsulate t1 compilation with an if checking whether it is desired to be compiled. In main.cpp: - Add font testing (warning: currently FAILS!!!) - Add some cleaning up

--- test/t1/main.cpp
+++ test/t1/main.cpp
@@ -14,13 +14,29 @@ int main() {
         return 1;
     }
 
+    Font* f = loadFont("test/resource/AIRSTREA.TTF", 24);
+    if (f == nullptr) {
+        std::cout << "AIRSTREA.TTF couldn't be loaded!" << std::endl;
+        return 1;
+    }
+
+    Color c = { 1, 0, 0, 0 };
+    Color bg = { .5, .5, .25, 1 };
+
     while (window.isRunning()) {
         window.pollEvents();
         window.clear();
 
         drawTexture(t, 0, 0, window.getWidth(), window.getHeight(), 0, NO_COLOR);
+        drawText("RW Engine", f, 0, window.getHeight() - 200, 800 / 3, 200,
+            0, c, BLENDED);
+        //drawText_shaded("t1", f, 0, window.getHeight() - 256, 800 / 16, 200, 0,
+        //    c, bg);
 
         window.update();
     }
+
+    deleteTexture(t);
+    deleteFont(f);
     return 0;
 }
