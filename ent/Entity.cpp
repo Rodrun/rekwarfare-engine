3e3c4f5775098ccba7485d1a79b60f0c1475d322
Juan Mendez <jmendg314@gmail.com>
2016-08-24 23:13:42 -0400

    Change collision detection code

--- ent/Entity.cpp
+++ ent/Entity.cpp
@@ -6,10 +6,8 @@ Entity::Entity(float x, float y, float w, float h) :
     x(x), y(y), w(w), h(h) { }
 
 bool Entity::collides(Entity e) {
-    // could possibly do the opposite and check if not touching
-    if ((e.x >= x && e.x <= x + w && e.y >= y && e.y <= y + h) ||
-        (e.x + e.w >= x && e.x + e.w <= x + w &&
-            e.y + e.h >= y && e.y + e.h <= y + h)) // TODO: Fix this.
+    if (x < e.x + e.w && x + w > e.x &&
+		y < e.y + e.h && h + y > e.y)
         return true;
     return false;
 }
